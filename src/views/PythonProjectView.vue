d1a74274b804ec7c6fb1d16c5ae233cc088dfcac
Benny <benny.van.meerbeeck@student.howest.be>
2023-11-07 14:46:51 +0100

    create python project vue

---
+++ src/views/PythonProjectView.vue
@@ -0,0 +1,15 @@
+<script setup>
+import ProjectCard from '../components/ProjectCard.vue'
+
+const convertingNumeral = '/assets/Images/talstelsels-converteren-screenshot.PNG';
+
+const colorClass = "python-overlay-color"
+</script>
+
+<template>
+    <ProjectCard project-name="Converting numeral systems"
+                 :picture-path= convertingNumeral 
+                 title-and-alt="Converting numeral systems"
+                 :style-class= colorClass />
+    
+</template>
