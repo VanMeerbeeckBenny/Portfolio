8709a72df3d52ed1460352af5d7333d7cfc10207
Benny <benny.van.meerbeeck@student.howest.be>
2023-11-07 12:46:34 +0100

    Refactor to component

--- src/views/CProjectView.vue
+++ src/views/CProjectView.vue
@@ -1,103 +1,35 @@
+<script setup>
+import ProjectCard from '../components/ProjectCard.vue'
+
+const ApiPath = '/assets/Images/api-screenshot.PNG';
+const XamarinPath = '/assets/Images/xamarin-screenshot.PNG';
+const CricketPath = '/assets/Images/cricketdarts-screenshot.PNG';
+const SpoticlonePath = '/assets/Images/spoticlone-screenshot.PNG';
+const colorClass = "c-sharp-overlay-color"
+</script>
+
 <template>
-    <div class="container">
-        <div class="overlay">
-            <div class="text-container">
-                <p>Web</p>
-                <p>API</p>
-            </div>
-        </div>
-        <div class="content">
-            <img src="../assets/Images/api-screenshot.PNG" title="API" alt="API" >
-        </div>
-    </div>
-    <div class="container">
-        <div class="overlay">
-            <div class="text-container">
-                <p>Xamarin</p>                
-            </div>
-        </div>
-        <div class="content">
-            <img src="../assets/Images/xamarin-screenshot.PNG" title="Memmory helper" alt="Memmory helper">
-        </div>
-    </div>
-    <div class="container">
-        <div class="overlay">
-            <div class="text-container">
-                <p>Blazor</p> 
-                <p>Cricketdarts</p>               
-            </div>
-        </div>
-        <div class="content">
-            <img src="../assets/Images/cricketdarts-screenshot.PNG" title="Cricketdarts" alt="Cricketdarts">
-        </div>
-    </div>    
-    <div class="container">
-        <div class="overlay">
-            <div class="text-container">
-                <p>Blazor</p>
-                <p>Spoticlone</p>
-            </div>
-        </div>
-        <div class="content">
-            <img src="../assets/Images/spoticlone-screenshot.PNG" title="Spoti clone" alt="Spoti clone">
-        </div>
-    </div>
+    <ProjectCard project-type="API"
+                 project-name="Cocktail"
+                 :picture-path= ApiPath 
+                 title-and-alt="API"
+                 :style-class= colorClass />
+
+    <ProjectCard project-type="Xamarin"
+                 project-name="Cocktail"
+                 :picture-path= XamarinPath 
+                 title-and-alt="Xamarin MemmoryHelper"
+                 :style-class= colorClass />
+
+    <ProjectCard project-type="Blazor"
+                 project-name="Cricketdarts"
+                 :picture-path= CricketPath 
+                 title-and-alt="Blazor Crickerdarts"
+                 :style-class= colorClass />
+
+    <ProjectCard project-type="Blazor"
+                 project-name="Spoticlone"
+                 :picture-path= SpoticlonePath 
+                 title-and-alt="Blazor Spoticlone"
+                 :style-class= colorClass />
 </template>
-<style scoped>
-.container{    
-    margin: 10px 10px;
-    padding: 0;
-    overflow: hidden;
-    border-radius: 25px; 
-    position: relative;
-    box-shadow: 10px 10px 15px black;    
-}
-.text-container{ 
-    text-align: center;
-}
-
-.overlay{
-    width: inherit;
-    height: inherit;
-    z-index: 1;
-    position: absolute;
-    content:" ";
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    background-color: rgb(100, 65, 134);
-    transition:opacity 1s ease-in-out;
-    
-}
-
-.overlay:hover{
-    opacity: 0;  
-}
-
-.overlay p {    
-    opacity: 0.5;
-    font-weight:bold;
-    font-style:italic;
-    font-size: 1rem;
-    margin: 0;  
-}
-.content img,.container{
-    height:150px;
-    width: 300px;
-}
-
-@media (min-width: 1024px){
-    .content img,.container{
-    height:250px;
-    width: 500px;
-  
-}
-    .container{
-        margin: 20px 20px;
-    }
-    .overlay p{
-        font-size: 3.5rem;
-        line-height: 60px;
-    }
-};
-</style>
