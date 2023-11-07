738046d7b64995a47ae2bf9eea5c2277f5694c02
Benny <benny.van.meerbeeck@student.howest.be>
2023-11-07 14:47:30 +0100

    Create vue project view

---
+++ src/views/VueProjectView.vue
@@ -0,0 +1,15 @@
+<script setup>
+import ProjectCard from '../components/ProjectCard.vue'
+
+const convertingNumeral = '/assets/Images/talstelsels-converteren-screenshot.PNG';
+
+const colorClass = "vue-overlay-color"
+</script>
+
+<template>
+    <ProjectCard project-name="Cocktails"
+                 :picture-path= convertingNumeral 
+                 title-and-alt="Cocktails"
+                 :style-class= colorClass />
+    
+</template>
