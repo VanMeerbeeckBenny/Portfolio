3b6f2f66573b5d421bf93538a8d3c653f5b3e6f7
Benny <benny.van.meerbeeck@student.howest.be>
2023-11-09 14:01:56 +0100

    Create return button component

---
+++ src/components/ReturnButton.vue
@@ -0,0 +1,22 @@
+<script setup>
+import {useRouter} from 'vue-router'
+const props = defineProps({
+    pageName:String
+})
+const router = useRouter();
+const returnToProjects = () => {
+    router.push({name:props.pageName})
+}
+</script>
+
+<template>
+<button class="btn btn-primary mt-3 mb-3"
+                    @click="returnToProjects">Back</button>
+</template>
+
+
+<style scoped>
+button{
+    width:200px;
+}
+</style>
