2c0c7641b35bf4ff3c19c333078e6681b276e060
Benny <benny.van.meerbeeck@student.howest.be>
2023-11-09 14:01:17 +0100

    Create video example component

---
+++ src/components/VideoExample.vue
@@ -0,0 +1,48 @@
+<script setup>
+const props = defineProps({
+    videoClipLink:String,
+    repositoryLink:String
+})
+</script>
+
+<template>
+    <div>
+        <iframe 
+                :src= props.videoClipLink               
+                allowfullscreen>
+        </iframe>
+        <p>Take a look at the source code <a :href=props.repositoryLink>here</a></p>
+    </div>
+</template>
+
+<style scoped>
+
+a{    
+    color:rgba(235, 64, 64, 0.651);    
+    text-decoration: underline;      
+}
+
+p{
+    font-size: 0.8rem; 
+}
+
+a:hover{
+    background-color: transparent;
+}
+iframe{
+    width:100%;
+    height:200px;
+}
+@media(min-width:1000px){
+    iframe{
+        margin-top:50px;
+        margin-right: 50px;
+        width:560px;
+        height:315px;
+    }  
+    
+    p{
+    font-size: 1rem; 
+    }
+}
+</style>
