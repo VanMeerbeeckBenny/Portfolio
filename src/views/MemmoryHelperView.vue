61fa6ae73e870b3691debd2f5eea7c264e18e70f
Benny <benny.van.meerbeeck@student.howest.be>
2023-11-20 14:55:42 +0100

    Refactor for readability

--- src/views/MemmoryHelperView.vue
+++ src/views/MemmoryHelperView.vue
@@ -13,29 +13,16 @@ const toolsUsed = ["C#","VS","FreshMvvm","Firebase","Github"]
                                 repository-link="#" >
 
     
-    <template #info-section>
-        <section class="tech-stack">                    
-                    <p class="stack-item">Framework:</p><p class="details">Xamarin (.NET Core) with FreshMvvm</p>       
-                    <p class="stack-item">Database:</p><p class="details"> Firebase Realtime Database</p>                     
-                    <p class="stack-item">User Management:</p><p class="details">A custom table within the Realtime Database features a userId column, aligning with the User ID (UID) found in the corresponding row from Firebase Authentication.</p>
-                    <p class="stack-item">Authentication & Authorization:</p><p class="details"> Firebase Authentication</p>
-                    <p class="stack-item">Dependency Injection:</p><p class="details"> Enhanced code flexibility and maintainability</p>                                    
-                </section>
+    <template #info-section>                       
+        <p class="stack-item">Framework:</p><p class="details">Xamarin (.NET Core) with FreshMvvm</p>       
+        <p class="stack-item">Database:</p><p class="details"> Firebase Realtime Database</p>                     
+        <p class="stack-item">User Management:</p><p class="details">A custom table within the Realtime Database features a userId column, aligning with the User ID (UID) found in the corresponding row from Firebase Authentication.</p>
+        <p class="stack-item">Authentication & Authorization:</p><p class="details"> Firebase Authentication</p>
+        <p class="stack-item">Dependency Injection:</p><p class="details"> Enhanced code flexibility and maintainability</p>  
     </template>
+    
     <template #return-button>
         <ReturnButton pageName="c-project-page"></ReturnButton>
     </template>
    </ProjectPresentationTemplate>
 </template>
-
-<style scoped>
-
-h4{    
- 
-    width:fit-content;
-    margin:0 0 20px 0;  
-    text-decoration: underline;
-    color: rgba(255, 255, 255, 0.81);
-}
-
-</style>
