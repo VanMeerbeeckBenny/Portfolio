2a6ef9652d242d46e95ee504134123ff0a75ad00
benny <benny.van.meerbeeck@student.howest.be>
2023-11-26 03:16:07 +0100

    Create spoticlone page

---
+++ src/views/projects/BlazorSpoticlone.vue
@@ -0,0 +1,22 @@
+<script setup>
+
+import ProjectPresentationTemplate from '../../components/ProjectPresentationTemplate.vue';
+const videoLink ="https://www.youtube.com/embed/itQLBGQyTX8";
+const toolsUsed = ["C#","VS","Blazor","Github","MS-SQL"]
+</script>
+
+<template>
+   <ProjectPresentationTemplate :tools-used="toolsUsed"
+                                header-text="Blazor Spoticlone"
+                                :video-clip-link= videoLink
+                                repository-link="#"
+                                return-route-name="c-project-page">
+
+    
+    <template #info-section>                       
+        <p class="stack-item">Framework:</p><p class="details">base on microsoft's ASP.NET Core 6</p>       
+        <p class="stack-item">Database:</p><p class="details"> MS-SQL server</p>  
+           <p class="stack-item">Dependency Injection:</p><p class="details"> Enhanced code flexibility and maintainability</p>  
+    </template> 
+   </ProjectPresentationTemplate>
+</template>
