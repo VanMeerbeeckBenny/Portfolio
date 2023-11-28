ebbb55130ab5880a252dccdecc3991b89b8789f5
Benny <benny.van.meerbeeck@student.howest.be>
2023-11-28 11:37:37 +0100

    Refactor for readability and stylng

--- src/views/projects/BlazorSpoticloneVue.vue
+++ src/views/projects/BlazorSpoticloneVue.vue
@@ -14,9 +14,25 @@ const toolsUsed = ["C#","VS","Blazor","Github","MS-SQL"]
 
     
     <template #info-section>                       
-        <p class="stack-item">Framework:</p><p class="details">base on microsoft's ASP.NET Core 6</p>       
+        <p class="stack-item">Framework:</p><p class="details">base on microsoft's ASP.NET Core</p>       
         <p class="stack-item">Database:</p><p class="details"> MS-SQL server</p>  
-           <p class="stack-item">Dependency Injection:</p><p class="details"> Enhanced code flexibility and maintainability</p>  
+        <p class="stack-item">Dependency Injection:</p><p class="details"> Enhanced code flexibility and maintainability</p> 
+        <div id="note">
+          <p class="stack-item">Note:</p><p class="details"> My focus was the web Blazor project.The other parts were given by the lector.</p> 
+        </div>
+       
     </template> 
    </ProjectPresentationTemplate>
 </template>
+<style scoped>
+#note p{
+   font-size: 0.5rem;
+   font-weight: bold;
+}
+@media (min-width:1024px) {
+#note p{
+   font-size: 0.7rem;
+   
+}
+}
+</style>
