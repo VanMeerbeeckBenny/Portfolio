78df00fb2c614ce7e4330ceac1248d201af2bed5
Benny <benny.van.meerbeeck@student.howest.be>
2023-12-04 21:27:25 +0100

    Refactor for readability and functionality

--- src/views/projects/PygameView.vue
+++ src/views/projects/PygameView.vue
@@ -39,8 +39,7 @@ const githubLink = "https://github.com/VanMeerbeeckBenny/Mario-inspired-game"
         <p class="stack-item">Physics Engine:</p><p class="details">Pymunk is used for the swinging physics</p> 
         <p class="stack-item">Map design:</p><p class="details">Tiled, a user-friendly map editor, is utilized for easily creating and overseeing the map</p>
         <p class="stack-item">Map integration:</p><p class="details">Pytmx is utilized for loading the map</p> 
-        <p class="stack-item">Environment Management:</p><p class="details">Miniconda is employed for managing the project's environment, ensuring a secure and controlled setup.</p>
-        <p class="stack-item">Note:</p><p class="details">As this project was created in school where legal issues were not a concern, I used many original assets. Due to copyright laws, all assets have been removed. Consequently, the game cannot be run, but you can explore the code.</p>
+        <p class="stack-item">Environment Management:</p><p class="details">Miniconda is great for managing the project's environment, ensuring a secure and controlled setup.</p>        
     </template> 
    </ProjectPresentationTemplate>
 </template>
@@ -49,7 +48,10 @@ const githubLink = "https://github.com/VanMeerbeeckBenny/Mario-inspired-game"
     width: 90%;
 }
 #legal p {
-    font-size: 0.7rem;
+    font-size: 0.6rem;        
+}
+#legal p,#legal h5 {     
+    color:rgba(255, 255, 255, 0.558);  
 }
 
 @media (min-width: 1024px){
@@ -57,7 +59,7 @@ const githubLink = "https://github.com/VanMeerbeeckBenny/Mario-inspired-game"
         width: 90%;
     }
     #legal p {
-        font-size: 0.8rem;
+        font-size: 0.8rem;        
     }
 }
 </style>
