6c971878d2fc086f91a6251bfc221652eddd7095
Benny <benny.van.meerbeeck@student.howest.be>
2023-12-05 13:38:03 +0100

    Fix issue layout

--- src/components/ProjectPresentationTemplate.vue
+++ src/components/ProjectPresentationTemplate.vue
@@ -55,7 +55,7 @@ export default {
 
 
 .info-container{
-    margin-top: 70px;
+    padding-top: 70px;
     min-height: 100vh;
     display: flex;
     justify-content: center;
@@ -91,7 +91,7 @@ h4{
 }
 @media (min-width: 1024px) {
 .info-container{
-    margin-top: 10px;
+    padding-top: 10px;
 }
 .tech-stack{
 max-width: 30%;
