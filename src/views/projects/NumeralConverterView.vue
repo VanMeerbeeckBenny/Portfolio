8d7a879b46ad70ad53ccfcd44bacd2d6d9703537
Benny <benny.van.meerbeeck@student.howest.be>
2023-12-08 12:56:34 +0100

    Create numeral convert view

---
+++ src/views/projects/NumeralConverterView.vue
@@ -0,0 +1,17 @@
+<script setup>
+import ProjectPresentationTemplate from '../../components/ProjectPresentationTemplate.vue';
+const videoLink ="https://www.youtube.com/embed/itQLBGQyTX8";
+const githubLink = "https://github.com/VanMeerbeeckBenny/Number-system-converter";
+const toolsUsed = ["JS","HTML","CSS","VS","Github"]
+const returnRoute = "javascript-project-page"
+</script>
+
+<template>
+   <ProjectPresentationTemplate header-text="Converting numeral systems"
+                                :video-clip-link= videoLink
+                                :repository-link=githubLink
+                                :tools-used=toolsUsed
+                                :return-route-name=returnRoute> 
+    
+   </ProjectPresentationTemplate>  
+</template>
