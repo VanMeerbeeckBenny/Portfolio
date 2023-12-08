43c96093fecd73f8251f098a843c531c8969d5ec
Benny <benny.van.meerbeeck@student.howest.be>
2023-12-08 12:56:17 +0100

    Add route names

--- src/views/JavaScriptProjectView.vue
+++ src/views/JavaScriptProjectView.vue
@@ -3,18 +3,21 @@ import ProjectCard from '../components/ProjectCard.vue'
 const convertingNumeral = '/assets/Images/talstelsels-converteren-screenshot.PNG'
 const GameOfThronePath = '/assets/Images/game-of-thrones-screenshot.PNG'
 const colorClass = "javascript-overlay-color"
+
 </script>
 
 <template>
     <ProjectCard project-name="Converting numeral systems"
                  :picture-path= convertingNumeral 
                  title-and-alt="Converting numeral systems"
-                 :style-class= colorClass />
+                 :style-class=colorClass
+                 route-name="project-numeral-converter" />
 
-    <ProjectCard project-name="Game of thrones"
+    <ProjectCard project-name="Wiki game of thrones"
                  :picture-path= GameOfThronePath 
                  title-and-alt="Wiki game of thrones"
-                 :style-class= colorClass />
+                 :style-class= colorClass 
+                 route-name="project-wiki-game-of-thrones" />
 
     
 </template>
